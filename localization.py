7a1d305411db1e82bf4ac32f8c26ecf0d415b626
Suleyman Ali <suleymanali@outlook.com>
2022-06-04 18:54:26 +0200

    Add Kalman filter to Motion Based Localization

--- localization.py
+++ localization.py
@@ -1,6 +1,5 @@
 import copy
 from abc import ABC, abstractmethod
-
 import filterpy.common
 import numpy as np
 from robots.robots import ConstantAccelerationRobot2D, RandomAccelerationRobot2D, ControlledRobot2D, TwoRobotSystem
@@ -92,12 +91,11 @@ class PositionTracking(BaseLocalization):
 
 
 class MotionBasedLocalization(BaseLocalization):
-    def __init__(self, robot_system: TwoRobotSystem, count=50):
+    def __init__(self, robot_system: TwoRobotSystem, count=50, kf=None):
         super().__init__(robot_system, count)
+        self.kf = kf
         self.localized = False
         self.prev_v = robot_system.get_v_measurement()
-
-        # TODO: noise and filtering
 
     def run(self):
         def find_max_similarity(prev_positions, new_positions):
@@ -119,17 +117,32 @@ class MotionBasedLocalization(BaseLocalization):
             self.measured_positions[i] = [measurement1, measurement2]
 
             if self.localized:
-                measured_pos = Util.closest_to(self.estimated_positions[i-1], [measurement1, measurement2])
-                self.estimated_positions[i] = measured_pos
+                # Compare measurements to moving average of previous positions
+                window_length = 5
+                window = list(range(max(self.idx_loc, i-window_length), i))
+                prev_pos = np.mean(self.estimated_positions[window], axis=0)
+                # prev_pos = self.estimated_positions[i-1]
+                closest_measurement = Util.closest_to(prev_pos, [measurement1, measurement2])
+                self.chosen_measurements[i] = closest_measurement
+
+                if self.kf:
+                    self.kf.predict()
+                    self.kf.update(closest_measurement)
+                    closest_measurement = [self.kf.x[0], self.kf.x[1]]
+                self.estimated_positions[i] = closest_measurement
             else:
                 similarity = Util.cos_similarity(self.prev_v, measured_v)
-                if similarity < .99:
+                if similarity < .99 and i > 1:
                     self.localized = True
                     prev1 = self.measured_positions[i-1][0]
                     prev2 = self.measured_positions[i-1][1]
 
                     max_pos = find_max_similarity([prev1, prev2], [measurement1, measurement2])
                     self.estimated_positions[i] = max_pos
+
+                    if self.kf:
+                        self.kf.x = np.concatenate((max_pos, measured_v))
+
                     self.chosen_measurements[i] = max_pos
                     self.idx_loc = i
             self.prev_v = measured_v
@@ -140,20 +153,37 @@ class MotionBasedLocalization(BaseLocalization):
 
 def run_motion_based_localization():
     p0 = [3., 2.]
+    v0 = [1, 1]
     u = [[1, 0]] * 100 + [[1, 2]] * 100
     dt = .1
     is_noisy = True
-    r_std = .001
+    r_std = .01
     v_std = 0
+    target_ax_std = 1.5
+    target_ay_std = 1
     # u = [[1, 0], [1, 0], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2],
     #      [1, 2], [1, 2]]
-    # cr = ControlledRobot2D(u, dt=dt, init_pos=p0)
-    # cr = ConstantAccelerationRobot2D(p0, [.1, .1], [.1, .1], dt=dt)
-    cr = RandomAccelerationRobot2D(p0, [1, 1], dt, ax_noise=1.5, ay_noise=1)
-    cr_anchor = RandomAccelerationRobot2D([0., 0.], [-1, 1], dt, ax_noise=1, ay_noise=1.5)
+    # target = ControlledRobot2D(u, dt=dt, init_pos=p0)
 
-    system = TwoRobotSystem(None, cr, noise=is_noisy, r_std=r_std, v_std=v_std)
-    loc = MotionBasedLocalization(system, len(u))
+    target = RandomAccelerationRobot2D(p0, v0, dt, ax_noise=target_ax_std, ay_noise=target_ay_std)
+    # anchor = RandomAccelerationRobot2D([0., 0.], [-1, 1], dt, ax_noise=1, ay_noise=1.5)
+
+    system = TwoRobotSystem(None, target, noise=is_noisy, r_std=r_std, v_std=v_std)
+
+    kf = filterpy.common.kinematic_kf(2, 1, dt, order_by_dim=False)
+    ax_var = target_ax_std ** 2
+    ay_var = target_ay_std ** 2
+    kf.Q = np.array([[dt ** 4 * ax_var / 4, 0, dt ** 3 * ax_var / 2, 0],
+                     [0, dt ** 4 * ay_var / 4, 0, dt ** 3 * ay_var / 2],
+                     [dt ** 3 * ax_var / 2, 0, dt ** 2 * ax_var, 0],
+                     [0, dt ** 3 * ay_var / 2, 0, dt ** 2 * ay_var]])
+    kf.P *= copy.deepcopy(kf.Q)
+
+    # kf.R = np.array([[23424.51598114,  9877.72176729], [9877.72176729,  5403.0224787]])
+    # for r_std = 0.1
+    kf.R = np.array([[2912.56428837, -1857.63133772], [-1857.63133772, 10249.59328298]])
+
+    loc = MotionBasedLocalization(system, 1000, kf)
     loc.run()
 
     rmse_est = Util.rmse(loc.estimated_positions[loc.idx_loc:], system.all_target_positions[loc.idx_loc:])
@@ -218,6 +248,39 @@ def run_position_tracking():
     loc.animate_results(title=title, save=False)
 
 
+def determine_r_matrix():
+    count = 10000
+    p0 = [3., 2.]
+    v0 = [1, 1]
+    dt = .1
+    is_noisy = True
+    r_std = .1
+    v_std = 0
+    target_ax_std = 1.5
+    target_ay_std = 1
+
+    reps = 1000
+    q = np.array([[0., 0.], [0., 0.]])
+    for i in range(reps):
+        target = RandomAccelerationRobot2D(p0, v0, dt, ax_noise=target_ax_std, ay_noise=target_ay_std)
+        system = TwoRobotSystem(None, target, noise=is_noisy, r_std=r_std, v_std=v_std)
+        loc = MotionBasedLocalization(system, count)
+        loc.run()
+
+        if loc.localized:
+            error = loc.chosen_measurements[loc.idx_loc:] - system.all_target_positions[loc.idx_loc:]
+            e_nx = np.mean(error[:][0]**2)
+            e_nx_ny = np.mean(error[:][0] * error[:][1])
+            e_ny = np.mean(error[:][1]**2)
+            q += np.array([[e_nx, e_nx_ny], [e_nx_ny, e_ny]])
+        else:
+            reps -= 1
+    q /= reps
+    print(reps)
+    return q
+
+
 if __name__ == '__main__':
     run_motion_based_localization()
     # run_position_tracking()
+    # print(determine_e_matrix())
